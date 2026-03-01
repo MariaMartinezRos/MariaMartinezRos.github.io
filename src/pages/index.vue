ff1814e011dcfe6ca902953b15dda0cfcad18291
MariaMartinezRos <cibernuggets@gmail.com>
2026-03-01 20:23:27 +0100

    route

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { R } from 'vue-router/dist/router-CWoNjPRp.mjs'
+</script>
 
 <template>
   <div class="flex flex-col items-center justify-center gap-4">
@@ -6,6 +8,7 @@
     <p class="text-lg text-gray-600">Welcome to the landing page of this application.</p>
     <RouterLink :to="{ name: '/HomeView' }"> Go Back Home </RouterLink>
     <RouterLink :to="{ name: '/dev/' }"> Developer tools </RouterLink>
+    <RouterLink :to="{ name: '/dev/', params: { Hola: 'gallery' } }"> Gallery </RouterLink>
     <RouterLink :to="{ name: '/cv/' }"> Contact me </RouterLink>
     <RouterLink :to="{ name: '/art/' }"> Art Gallery </RouterLink>
   </div>
